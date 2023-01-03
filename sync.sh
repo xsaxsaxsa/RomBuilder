30861f74a099786cc228d17e3c181cacaf4d99e2
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-03 21:33:22 +0530

    Clone Custom
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -17,5 +17,13 @@ git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Devi
 git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
 
+# Custom
+rm -rf hardware/xiaomi
+rm -rf hardware/qcom-caf/wlan
+rm -rf hardware/qcom-caf/bootctrl
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+git clone --depth=1 https://github.com/Jabiyeff-Project/android_hardware_qcom_wlan.git -b 13.0-spes hardware/qcom-caf/wlan
+git clone --depth=1 https://github.com/ArrowOS/android_hardware_qcom_bootctrl.git -b arrow-13.0-caf hardware/qcom-caf/bootctrl
+
 # Exit
 exit 0
