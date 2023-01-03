35e6d87003e379baad11dcc4312d03a7a39ddc05
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2023-01-03 22:17:09 +0530

    Build crDroid
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -7,10 +7,10 @@ mkdir -p $SYNC_PATH
 cd $SYNC_PATH
 
 # Init Repo
-repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
+repo init -u $MANIFEST -b $MANIFEST_BRANCH
 
 # Sync the Sources
-repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
+repo sync
 
 # Clone Trees
 git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
@@ -21,6 +21,7 @@ git clone --depth=1 $KT_LINK $KT_PATH
 rm -rf hardware/xiaomi
 rm -rf hardware/qcom-caf/wlan
 rm -rf hardware/qcom-caf/bootctrl
+rm -rf hardware/google/pixel/kernel_headers
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 git clone --depth=1 https://github.com/Jabiyeff-Project/android_hardware_qcom_wlan.git -b 13.0-spes hardware/qcom-caf/wlan
 git clone --depth=1 https://github.com/ArrowOS/android_hardware_qcom_bootctrl.git -b arrow-13.0-caf hardware/qcom-caf/bootctrl
